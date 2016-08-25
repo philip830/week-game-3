691bd8143b0618375260a531c09898f9e1f40ed8
Philip Graham <Philip Graham>
2016-08-25 12:42:17 -0400

    Ver. 3 - Hangman

---
+++ assets/javascript/backupscripts.js
@@ -0,0 +1,5 @@
+ var x = document.createElement("AUDIO"); {
+		        x.setAttribute("src","http://jugnifm.com/music/temp320/Mississippi_Queen_-_[JugniFm.CoM].mp3yourAudioTag");
+		    } 
+		    x.setAttribute("controls", "controls");
+		    document.body.appendChild(x);
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,7 @@
-// Global Variables
+// Global Variables{{
 // *********************************************
 
-var wordOptions = ["dog", "cat", "bird", "kangaroo", "snake", "turtle"];
+var wordOptions = ["aerosmith", "mountain", "queen", "skylark", "argent", "trex"];
 var selectedWord = "";
 var lettersinWord = [];
 var numBlanks = 0;
@@ -28,8 +28,8 @@ function startGame () {
 
 	for (var i=0; i<numBlanks; i++){
 		blanksAndSuccesses.push("_");
-	}	
-
+	}
+	
 	//Change HTML to reflect round condition
 	document.getElementById("wordToGuess").innerHTML = blanksAndSuccesses.join("  ");
 	document.getElementById("numGuesses").innerHTML = guessesLeft;
@@ -56,34 +56,32 @@ function checkLetters(letter) {
 	if (isLetterInWord) {
 		for (var i=0; i<numBlanks; i++) {
 			if(selectedWord[i] == letter) {
-				blanksAndSuccesses[i] = letter;
+				blanksAndSuccesses[i] =	 letter;
 			}
 		}
 	}
 
-//Letter not found
+	//Letter not found
 	else {
 		wrongLetters.push(letter);
 		guessesLeft--
-	}
-
-	console.log(blanksAndSuccesses);
-
+		}
 }	
 
 function roundComplete() {
-	console.log("Win Count: " + winCount + " | Loss Count: " + lossCount + " |Guesses Left " + guessesLeft);
+	console.log("Win Count: " + winCount + " | Loss Count: " + lossCount + " | Guesses Left " + guessesLeft);
 
 	//Update the HTML to reflect current count stats
-	document.getElementById("guessesLeft").innerHTML = guessesLeft;
+	document.getElementById("numGuesses").innerHTML = guessesLeft;
 	document.getElementById("wordToGuess").innerHTML = blanksAndSuccesses.join("  ");
-	document.getElementById("wrongLetters").innerHTML = wrongGuesses.join("  ");
-	//Check if win :)
-	console.log("In win")
-	if (lettersinWord.toString() == blanksAndSuccesses.toString()) {
-		winCount++;
-		alert("You win");
+	document.getElementById("wrongGuesses").innerHTML = wrongLetters.join("  ");
 
+	//Check if win :)
+	console.log("in Win")	
+	if (lettersinWord.toString() == blanksAndSuccesses.toString()) {
+		winCount++; 
+		alert("Far out! You won, cool cat (if male) fine lady (if female)");
+											
 		//Update HTML win counter
 		document.getElementById("winCounter").innerHTML = winCount;
 			
@@ -93,7 +91,7 @@ function roundComplete() {
 	//Check if lose :(
 	else if (guessesLeft == 0) {
 		lossCount++;
-		alert("You lose");
+		alert("Heavy scene, man. You lose.");
 
 		//Update HTML loss counter
 		//Update HTML win counter
@@ -103,7 +101,19 @@ function roundComplete() {
 	}
 }
 
-	
+//function myFunction() {
+	//	    var x = document.createElement("AUDIO");
+
+	//	    if (x.canPlayType("audio/mpeg")) {
+	//	        x.setAttribute("src","http://jugnifm.com/music/temp320/Mississippi_Queen_-_[JugniFm.CoM].mp3yourAudioTag");
+	//	    } 
+	//	    x.setAttribute("controls", "controls");
+	//	    document.body.appendChild(x);
+		
+//function updateSource(){ 
+//    var audio = document.getElementById('http://jugnifm.com/music/temp320/Mississippi_Queen_-_[JugniFm.CoM].mp3yourAudioTag');
+//        audio.load(); 
+//}	
 
 
 // Main Process
@@ -116,6 +126,7 @@ document.onkeyup = function (event) {
 	var letterGuessed = String.fromCharCode(event.keyCode).toLowerCase();
 	checkLetters(letterGuessed);
 	roundComplete();
+	//myFunction();
+	//updateSource();
 	//Testing this code//
-	console.log(letterGuessed);
 }
